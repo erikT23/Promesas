6e9c6d4c3825a077d67d7639c30341cf124c1dda
erikT23 <20203tn152@utez.edu.mx>
2023-05-09 19:35:13 -0600

    promesas2

--- 02-promesas.js
+++ 02-promesas.js
@@ -1,17 +1,35 @@
-console.log("hola mundo");
-
-function sumarUno(numero, callback) {}
-
 function sumarUnoPromesa(numero) {
+  console.log(numero);
   var promesa = new Promise(function (resolve, reject) {
     if (numero >= 7) {
-      callback("numero muy grande");
+      reject("numero muy grande");
       return;
     }
 
     setTimeout(function () {
-      callback(null, numero + 1);
-    }, 2000);
+      resolve(numero + 1);
+    }, 1000);
   });
   return promesa;
 }
+
+sumarUnoPromesa(5)
+  .then((nuevoValor) => {
+   
+    return sumarUnoPromesa(nuevoValor);
+  })
+  .then((nuevoValor) => sumarUnoPromesa(nuevoValor))
+  .then(sumarUnoPromesa)
+  .catch((err) => {
+    console.log(err);
+  });
+
+// sumarUnoPromesa(5).then(function (nuevoValor) {
+//   console.log(nuevoValor);
+//   sumarUnoPromesa(nuevoValor).then(function (nuevoValor) {
+//     console.log(nuevoValor);
+//     sumarUnoPromesa(nuevoValor).then(function (nuevoValor) {
+//       console.log(nuevoValor);
+//     });
+//   });
+// });
