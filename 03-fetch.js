d098e9f14cadaa406cf6b21c2a4ab761c1e62cbe
erikT23 <20203tn152@utez.edu.mx>
2023-05-11 16:21:57 -0600

    cosas

--- 03-fetch.js
+++ 03-fetch.js
@@ -15,7 +15,7 @@ function cargarCards() {
             <h5 class="card-title">Name <span id="nombre">${data.data[i].first_name}</span></h5>
             <p class="card-text">Apellido:<span> ${data.data[i].last_name}</span>
             </p>
-            <button  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button onclick="cargarModal(this.value)" value ="${data.data[i].id}" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Detalles
 </button>
           </div>
@@ -29,18 +29,23 @@ function cargarCards() {
     .catch((err) => console.log("error", err));
 }
 
-function cargarModal() {
-  const rep = fetch("https://reqres.in/api/users?page=1");
-  rep
+
+function cargarModal(id) {
+
+  var userId = id;
+
+  const rep2 = fetch("https://reqres.in/api/users/" + userId + "?");
+
+  console.log(rep2);
+  rep2
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
       var mostrar = document.getElementById("modal");
-      for (var i = 0; i < data.data.length; i++) {
-        var create = `<
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      var create = `
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
@@ -48,25 +53,22 @@ function cargarModal() {
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <img src="${data.data[i].avatar}" class="img-thumbnail" alt="...">
-              <h5 class="card-title">Name <span id="nombre">${data.data[i].first_name}</span></h5>
-              <p class="card-text">Apellido:<span> ${data.data[i].last_name}</span>
+              <img src="${data.data.avatar}" class="img-thumbnail" alt="...">
+              <h5 class="card-title">Name <span id="nombre">${data.data.first_name}</span></h5>
+              <p class="card-text">Apellido:<span> ${data.data.last_name}</span>
               </p>
-              <p class="card-text">Email:<span> ${data.data[i].email}</span>
+              <p class="card-text">Email:<span> ${data.data.email}</span>
               </p>
             </div>
             <div class="modal-footer">
-              <button <button onclick="cargarModal()" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-             
+              <button  type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
           </div>
         </div>
       </div>
-  
-  
     `;
-        mostrar.insertAdjacentHTML("beforeend", create);
-      }
+      mostrar.insertAdjacentHTML("beforeend", create);
     })
     .catch((err) => console.log("error", err));
 }
+
